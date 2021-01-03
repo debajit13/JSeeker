8b0142f3d4bd88431520b483aa9bbc2a8f423321
Sankalan Dasgupta <43284744+Sankalan47@users.noreply.github.com>
2021-01-03 20:55:25 +0530

    add company lists

--- src/components/Table/data.js
+++ src/components/Table/data.js
@@ -168,6 +168,198 @@ const data = [
     glassdoorURL:
       "https://www.glassdoor.co.in/Reviews/Cure-Fit-Reviews-E1675320.htm",
   },
+  {
+    companyName: "Wallmart",
+    carrearURL: "https://careers.walmart.com/",
+    glassdoorURL:
+      "https://www.glassdoor.co.in/Reviews/Walmart-Reviews-E715.htm",
+  },
+  {
+    companyName: "Ola",
+    carrearURL: "https://www.olacabs.com/careers",
+    glassdoorURL:
+      "https://www.glassdoor.co.in/Reviews/Ola-Reviews-E587383.htm",
+  },
+  {
+    companyName: "Ola",
+    carrearURL: "https://www.olacabs.com/careers",
+    glassdoorURL:
+      "https://www.glassdoor.co.in/Reviews/Ola-Reviews-E587383.htm",
+  },
+  {
+    companyName: "ShareChat",
+    carrearURL: "https://we.sharechat.com/careers",
+    glassdoorURL:
+      "https://www.glassdoor.co.in/Reviews/ShareChat-Reviews-E1776417.htm",
+  },
+  {
+    companyName: "Udaan",
+    carrearURL: "https://careers.udaan.com/#!/",
+    glassdoorURL:
+      "https://www.glassdoor.co.in/Reviews/udaan-com-Reviews-E2023654.htm",
+  },
+  {
+    companyName: "Nutanix",
+    carrearURL: "https://www.nutanix.com/company/careers",
+    glassdoorURL:
+      "https://www.glassdoor.co.in/Reviews/Nutanix-Reviews-E429159.htm",
+  },
+  {
+    companyName: "Informatica",
+    carrearURL: "https://informatica.gr8people.com/",
+    glassdoorURL:
+      "https://www.glassdoor.co.in/Reviews/Informatica-Reviews-E8797.htm",
+  },
+  {
+    companyName: "Tower Research",
+    carrearURL: "https://informatica.gr8people.com/",
+    glassdoorURL:
+      "https://www.glassdoor.co.in/Reviews/Informatica-Reviews-E8797.htm",
+  },
+  {
+    companyName: "Intel Corporation",
+    carrearURL: "https://jobs.intel.com/",
+    glassdoorURL:
+      "https://www.glassdoor.co.in/Reviews/Intel-Corporation-Reviews-E1519.htm",
+  },
+  {
+    companyName: "Paypal",
+    carrearURL: "https://www.paypal.com/in/webapps/mpp/jobs",
+    glassdoorURL:
+      "https://www.glassdoor.co.in/Reviews/PayPal-Reviews-E9848.htm",
+  },
+  {
+    companyName: "Yelp",
+    carrearURL: "https://www.yelp.careers/us/en",
+    glassdoorURL:
+      "https://www.glassdoor.co.in/Reviews/Yelp-Reviews-E43314.htm",
+  },
+  {
+    companyName: "ThoughtWorks",
+    carrearURL: "https://www.thoughtworks.com/careers/jobs",
+    glassdoorURL:
+      "https://www.glassdoor.co.in/Reviews/ThoughtWorks-Reviews-E38334.htm",
+  },
+  {
+    companyName: "Zomato",
+    carrearURL: "https://zomato.recruitee.com/",
+    glassdoorURL:
+      "https://www.glassdoor.co.in/Reviews/Zomato-Reviews-E515676.htm",
+  },
+  {
+    companyName: "ByteDance",
+    carrearURL: "https://jobs.bytedance.com/en/",
+    glassdoorURL:
+      "https://www.glassdoor.co.in/Reviews/ByteDance-Reviews-E1624196.htm",
+  },
+  {
+    companyName: "Makemytrip",
+    carrearURL: "https://careers.makemytrip.com/",
+    glassdoorURL:
+      "https://www.glassdoor.co.in/Reviews/MakeMyTrip-Reviews-E354629.htm",
+  },
+  {
+    companyName: "Expedia",
+    carrearURL: "https://lifeatexpediagroup.com/",
+    glassdoorURL:
+      "https://www.glassdoor.co.in/Reviews/Expedia-Group-Reviews-E9876.htm",
+  },
+  {
+    companyName: "Postman",
+    carrearURL: "https://www.postman.com/company/careers/",
+    glassdoorURL:
+      "https://www.glassdoor.co.in/Reviews/Postman-Bangalore-Reviews-EI_IE1926052.0,7_IL.8,17_IM1091.htm?filter.iso3Language=eng",
+  },
+  {
+    companyName: "Sprinklr",
+    carrearURL: "https://www.sprinklr.com/careers",
+    glassdoorURL:
+      "https://www.glassdoor.co.in/Reviews/Sprinklr-Reviews-E427532.htm",
+  },
+  {
+    companyName: "Swiggy",
+    carrearURL: "https://careers.swiggy.com/#/",
+    glassdoorURL:
+      "https://www.glassdoor.co.in/Reviews/Swiggy-Reviews-E952680.htm",
+  },
+  {
+    companyName: "Innovaccer",
+    carrearURL: "https://innovaccer.com/careers/",
+    glassdoorURL:
+      "https://www.glassdoor.co.in/Reviews/Innovaccer-Reviews-E721552.htm",
+  },
+  {
+    companyName: "Mastercard",
+    carrearURL: "https://www.mastercard.us/en-us/vision/who-we-are/careers.html",
+    glassdoorURL:
+      "https://www.glassdoor.co.in/Reviews/Mastercard-Reviews-E3677.htm",
+  },
+  {
+    companyName: "American Express",
+    carrearURL: "https://jobs.americanexpress.com/india",
+    glassdoorURL:
+      "https://www.glassdoor.co.in/Reviews/American-Express-Reviews-E35.htm",
+  },
+  {
+    companyName: "Visa",
+    carrearURL: "https://www.visa.co.in/careers.html",
+    glassdoorURL:
+      "https://www.glassdoor.co.in/Reviews/Visa-Inc-Reviews-E3035.htm",
+  },
+  {
+    companyName: "ServiceNow",
+    carrearURL: "https://www.servicenow.co.uk/careers.html",
+    glassdoorURL:
+      "https://www.glassdoor.co.in/Reviews/ServiceNow-Reviews-E403326.htm",
+  },
+  {
+    companyName: "Oracle",
+    carrearURL: "https://www.oracle.com/in/corporate/careers/",
+    glassdoorURL:
+      "https://www.glassdoor.co.in/Reviews/Oracle-Reviews-E1737.htm",
+  },
+  {
+    companyName: "Dream 11",
+    carrearURL: "https://about.dream11.in/careers",
+    glassdoorURL:
+      "https://www.glassdoor.co.in/Reviews/Dream11-Mumbai-Reviews-EI_IE493359.0,7_IL.8,14_IM1070.htm?filter.iso3Language=eng",
+  },
+  {
+    companyName: "Vmware",
+    carrearURL: "https://careers.vmware.com/main/",
+    glassdoorURL:
+      "https://www.glassdoor.co.in/Reviews/VMware-Reviews-E12830.htm",
+  },
+  {
+    companyName: "Western Digital",
+    carrearURL: "https://jobs.westerndigital.com/",
+    glassdoorURL:
+      "https://www.glassdoor.co.in/Reviews/Western-Digital-Reviews-E1067.htm",
+  },
+  {
+    companyName: "Rakuten",
+    carrearURL: "https://rakuten.careers/",
+    glassdoorURL:
+      "https://www.glassdoor.co.in/Reviews/Rakuten-Reviews-E40197.htm",
+  },
+  {
+    companyName: "Facebook",
+    carrearURL: "https://www.facebook.com/careers/",
+    glassdoorURL:
+      "https://www.glassdoor.co.in/Reviews/Facebook-Reviews-E40772.htm",
+  },
+  {
+    companyName: "Google",
+    carrearURL: "https://careers.google.com/d/",
+    glassdoorURL:
+      "https://www.glassdoor.co.in/Reviews/Google-Reviews-E9079.htm",
+  },
+  {
+    companyName: "Amazon",
+    carrearURL: "https://www.amazon.jobs/en-gb/",
+    glassdoorURL:
+      "https://www.glassdoor.co.in/Reviews/Amazon-Reviews-E6036.htm",
+  },
 ];
 
 export default data;
